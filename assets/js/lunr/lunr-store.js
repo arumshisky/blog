a93771365e5097368ba99fc148274f7fb29434d2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-03 14:03:02 +0000

    Deploy annargrs/blog to github.com/annargrs/blog.git:gh-pages

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,26 @@
+var store = [{
+        "title": "Why blog about NLP in 2019?",
+        "excerpt":"When I said I wanted to start a research blog, I got asked why - wasn’t it something from the nineties that nobody did anymore? That’s actually not an unreasonable question: nobody is even able to keep up with arXiv anymore, why introduce any more sources of long reads? Sure,...","categories": ["squib"],
+        "tags": ["academia"],
+        "url": "https://hackingsemantics.xyz/2019/why-blog/",
+        "teaser":null},{
+        "title": "How the Transformers broke NLP leaderboards",
+        "excerpt":"This post summarizes some of the recent XLNet-prompted discussions on Twitter and offline. Idea credits go to Yoav Goldberg, Sam Bowman, Jason Weston, Alexis Conneau, Ted Pedersen, fellow members of Text Machine Lab, and many others. Any misconfiguration of those ideas is my own. A big reason why NLP is...","categories": ["squib"],
+        "tags": ["academia","methodology"],
+        "url": "https://hackingsemantics.xyz/2019/leaderboards/",
+        "teaser":null},{
+        "title": "On word analogies and negative results in NLP",
+        "excerpt":"In real world, fake news spread faster than facts. People’s attention is caught by sensational, exaggerated, clickbait-y messages like “5.23 million more immigrants are moving to the UK”. Any subsequent fact-checking messages look less sensational and they will not reach as many people. Once the damage is done, it’s done....","categories": ["squib"],
+        "tags": ["academia","methodology","negresults","review"],
+        "url": "https://hackingsemantics.xyz/2019/analogies/",
+        "teaser":null},{
+        "title": "Talking to people outside your echo chamber: SocNLP challenges",
+        "excerpt":"This blog is NOT political, even though the story below will show some of my political views. I’m sharing it because it highlights a burning issue in computational social science that is getting nowhere near enough attention. Reader beware. Academics and Populism Like most researchers I know, I’ve been politically...","categories": ["squib"],
+        "tags": ["academia","socialNLP"],
+        "url": "https://hackingsemantics.xyz/2019/conversation/",
+        "teaser":null},{
+        "title": "How to teach NLP to non-CS-majors in 2 weeks?",
+        "excerpt":"I strongly believe that getting machines to understand natural language, if at all possible, will require much interdisciplinary collaboration. It’s not clear whether NLP models should be biologically plausible or explicitly encode any linguistic structures – but we do know that language is a very complex thing, and no discipline...","categories": ["squib"],
+        "tags": ["academia","teaching"],
+        "url": "https://hackingsemantics.xyz/2019/nlp4linguists/",
+        "teaser":null}]
